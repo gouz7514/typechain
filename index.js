08807319108bcafb9f4bcfb1f7d9f59999983421
HOTJAE <gouz7514@naver.com>
2021-08-05 18:45:14 +0900

    update : type in typescript and tsc-watch

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "KHJ", age = 26, gender = "male";
 const sayHi = (name, age, gender) => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
-sayHi(name, age);
+sayHi("KHJ", 123, "male");
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string):string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+}
+
+sayHi("KHJ", 12, "male");
+
+export {};
